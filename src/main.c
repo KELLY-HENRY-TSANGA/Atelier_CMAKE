a2d3df6b7db47c8d050a084f6250a45dc9efc1a7
kELLY-HENRY-TSANGA <tsangakelly06@gmail.com>
2026-03-02 12:09:42 +0100

    Ajout de la fonction car

--- src/main.c
+++ src/main.c
@@ -4,33 +4,34 @@
 #include "./../include/calculator.h"
 
 int main(int argc, char *argv[]){
-	if (argc == 4)
+	if (argc == 4 || argc == 3)
 	{
-	char* op = argv[1];
-	char* a = argv[2];
-	char* b = argv[3];
-	double r = 0;
-	if (strcmp(op, "add") == 0) {
-		r = _add(atof(a), atof(b));
-		printf("%lf",r);
+		char* op = argv[1];
+		double r = 0;
+
+		if (strcmp(op, "add") == 0) {
+			r = _add(atof(argv[2]), atof(argv[3]));
+			printf("%lf", r);
+		}
+		else if (strcmp(op, "sub") == 0) {
+			r = _sub(atof(argv[2]), atof(argv[3]));
+			printf("%lf", r);
+		}
+		else if (strcmp(op, "mul") == 0) {
+			r = _mul(atof(argv[2]), atof(argv[3]));
+			printf("%lf", r);
+		}
+		else if (strcmp(op, "div") == 0) {
+			r = _div(atof(argv[2]), atof(argv[3]));
+			printf("%lf", r);
+		}
+		else if (strcmp(op, "car") == 0) {   // ← ICI, bien à l'intérieur
+			r = _car(atof(argv[2]));
+			printf("%lf", r);
+		}
+		else { printf("Erreur de parametres"); }
 	}
-	else if (strcmp(op, "sub") == 0) {
-		r = _sub(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "mul") == 0) {
-		r = _mul(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "div") == 0) {
-		r = _div(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else {printf("Erreur de parametres");}
-	}
-	else {printf("Erreur de parametres");}
-	return 0;
-}
-else if (strcmp(argv[1], "car") == 0) {
-    printf("%g\n", _car(atof(argv[2])));
-}	
+	else { printf("Erreur de parametres"); }
+
+	return 0;   // ← return 0 tout à la fin
+}              // ← accolade fermante du main tout à la fin
